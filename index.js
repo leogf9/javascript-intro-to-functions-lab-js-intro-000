c234081b5e03115fbfb5b7bb90e2d1662be7aa7f
Leonardo Gadotti de Figueiredo <leonardo.of.g@gmail.com>
2019-09-13 16:31:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if(string.toLowerCase)
-  return("I can't hear you!")
-}
-function sayHiToGrandma(string) {
-  if(string.toUpperCase)
-  return("YES INDEED!")
+  if(string.toLowerCase) {
+    return("I can't hear you!")
+  } else if (string.toUpperCase) {
+    return("YES INDEED!")
+  }
 }
