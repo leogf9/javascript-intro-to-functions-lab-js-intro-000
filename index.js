8c2813d3f0858d0d7347294141f50a53f359f41b
Leonardo Gadotti de Figueiredo <leonardo.of.g@gmail.com>
2019-09-13 16:46:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function sayHiToGrandma(string) {
   return "YES INDEED!";
     else if (string === "I love you, Grandma.")
     return "I love you, too.";
-    else if (string === string.toLowerCase)
+    else if (string === string.toLowerCase())
     return ("I can't hear you!");
 }
