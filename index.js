ffc334b0297575d123db2582831648fa1a3a5aec
Leonardo Gadotti de Figueiredo <leonardo.of.g@gmail.com>
2019-09-13 16:45:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if(string === string.toUpperCase()) 
+  if(string === string.toUpperCase())
   return "YES INDEED!";
-    else if (string === "I love you, Grandma.") 
+    else if (string === "I love you, Grandma.")
     return "I love you, too.";
     else if (string === string.toLowerCase)
     return ("I can't hear you!");
