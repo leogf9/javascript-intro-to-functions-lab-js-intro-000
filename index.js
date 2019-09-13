c620773dad526c1889832310e012a1d30670bdae
Leonardo Gadotti de Figueiredo <leonardo.of.g@gmail.com>
2019-09-13 16:39:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,16 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if(string.toLowerCase) {
-    return("I can't hear you!")
-  } else if (string.toUpperCase) {
-    return("YES INDEED!")
+  switch(string) {
+    case string.toUpperCase:
+    return ("YES INDEED!")
+    break;
+    case string.toLowerCase:
+    return ("I can't hear you!")
+    break;
+    case strig === "I love you":
+    return ("I love you, too")
+    break;
+    default: " "
   }
-}
+  
